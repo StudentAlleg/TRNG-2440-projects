259cb3820e79ecd2234b1e076eb9f07831c2e187
StudentAlleg <studentalleg-pilot@yahoo.com>
2026-06-24 09:04:08 -0700

    Project 1 - Fix create database

--- project_1/sql/create_database.sql
+++ project_1/sql/create_database.sql
@@ -1,14 +1,13 @@
---TODO
-CREATE TABLE IF NOT EXISTS Locations (
+CREATE TABLE IF NOT EXISTS Location (
     id          int           AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(32)        NOT NULL,
     latitude    DOUBLE             NOT NULL,
     longitude   DOUBLE             NOT NULL,
     UNIQUE(latitude, longitude)
-)
+);
 
-CREATE INDEX idx_latlong_locations on Locations (latitude, longitude)
-CREATE INDEX idx_id_locations on Locations (id)
+CREATE INDEX idx_latlong_locations on Location (latitude, longitude);
+CREATE INDEX idx_id_locations on Location (id);
 
 CREATE TABLE IF NOT EXISTS LocationData (
     location_id                 int         NOT NULL,
@@ -33,10 +32,11 @@ CREATE TABLE IF NOT EXISTS LocationData (
     daylight_duration           float       NOT NULL,
     sunset                      timestamp   NOT NULL,
     sunrise                     timestamp   NOT NULL,
+    PRIMARY KEY (location_id, day),
+    CONSTRAINT FK_location_id
+    FOREIGN KEY (location_id)
+    REFERENCES Location(id)
+);
 
-    FOREIGN KEY locationid REFERENCES Locations(ID),
-    PRIMARY KEY (locationid, day)
-)
-
-CREATE INDEX idx_location_locationdata on LocationData (id)
-CREATE INDEX idx_day_locationdata on Locations (id)
+CREATE INDEX idx_location_locationdata on LocationData (id);
+CREATE INDEX idx_day_locationdata on Locations (id);
