efc9355bf3a4fb5412970e473fdd2e7df8fe6005
StudentAlleg <studentalleg-pilot@yahoo.com>
2026-07-03 17:53:28 -0700

    Project 1 - update total daylight, add total rain fall, avg windspeed direction, and frequency of weather codes

--- project_1/sql/analytical_queries.sql
+++ project_1/sql/analytical_queries.sql
@@ -25,11 +25,36 @@ WHERE Weather.day > '2026-05-06'
 ORDER BY Weather.day ASC, Location.latitude ASC
 ;
 
---total daylight per day per location
-SELECT Location.name, Weather.day, Weather.sunset - Weather.sunrise as total_daylight FROM Weather
+--total daylight per day per location,
+--https://collectingwisdom.com/postgresql-convert-seconds-to-hhmmss/
+SELECT Location.name, Weather.day, Weather.sunset - Weather.sunrise as total_daylight, Weather.sunshine_duration * interval '1 sec' AS sunshine_time, Weather.daylight_duration * interval '1 sec' AS daylight_time FROM Weather
 JOIN Location
     ON Weather.location_id = Location.location_id
 ORDER BY Weather.day ASC, Location.latitude ASC
 ;
 
+--total precipitation hours and total rain_fall
+SELECT Location.name, SUM(Weather.precipitation_hours) as total_precipitation_hours, SUM(Weather.rain_sum) as total_rain_sum FROM Weather
+JOIN Location
+    ON Weather.location_id = Location.location_id
+GROUP BY Location.location_id, Location.name
+ORDER BY Location.latitude ASC
+;
+
+
+--avg windspeed direction
+SELECT Location.name, AVG(wind_direction_10m_dominant) as avg_wind_direction FROM Weather
+JOIN Location
+    ON Weather.location_id = Location.location_id
+GROUP BY Location.location_id, Location.name
+ORDER BY Location.latitude ASC
+;
+
+--Frequency of weather codes per location
+SELECT Location.name, Weather.weather_code, COUNT(*) as weather_code_frequency FROM Weather
+JOIN Location
+    ON Weather.location_id = Location.location_id
+GROUP BY Location.location_id, Location.name, Weather.weather_code
+ORDER BY Location.latitude ASC, weather_code_frequency DESC
+;
 
